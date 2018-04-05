73ff80309d9889a00261f69c0b97212224ed5ef3
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-05 01:33:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,10 +3,10 @@ require "pry"
 
 
 
-# def reverse_each_word(string)
-#   string.split(" ").collect do |word|
-#   word.reverse
-#   # biding.pry
-#   end
-#   string
-# end
+def reverse_each_word(string)
+  string.split(" ").collect do |word|
+  word.reverse
+  # biding.pry
+  end
+  string
+end
