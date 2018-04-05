1b480810db924b1f87efbe2636936b518d16b5e6
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-05 01:30:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 require "pry"
 
-def reverse_each_word(string)
-  reversed = []
-  string.split(" ").each do |word|
-  reversed << word.reverse
-  end
-  reversed.join(" ")
-end
+# def reverse_each_word(string)
+#   reversed = []
+#   string.split(" ").each do |word|
+#   reversed << word.reverse
+#   end
+#   reversed.join(" ")
+# end
 
 
 def reverse_each_word(string)
